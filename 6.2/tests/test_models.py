8bf004b040ba5b2ec658118901f35272347cc8cf
Roberto-San85 <a00903762@tec.mx>
2026-02-21 23:22:44 +0000

    test(hotel): se corrige orden de parámetros en assertEqual

--- 6.2/tests/test_models.py
+++ 6.2/tests/test_models.py
@@ -5,8 +5,8 @@ from src.reservation_system.models import Customer, Hotel
 class TestHotel(unittest.TestCase):
     def test_hotel_create_valid(self):
         h = Hotel.create("Hotel Test", 3)
-        self.assertEqual(h.name, "Hotel Test")
-        self.assertEqual(h.total_rooms, 3)
+        self.assertEqual("Hotel Test", h.name)
+        self.assertEqual(3, h.total_rooms)
         self.assertTrue(h.id)
 
     def test_hotel_create_invalid(self):
